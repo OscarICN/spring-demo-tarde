9cf6183d694076bf93910e29af79380b228ea83e
OscarICN <oscar.ivan.c.n@gmail.com>
2022-05-05 20:24:22 -0500

    Iniciando el proyecto con Spring Boot

---
+++ src/main/java/com/ibm/academia/springdemotarde/SpringDemoTardeApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.springdemotarde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoTardeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoTardeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/academia/springdemotarde/SpringDemoTardeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.springdemotarde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoTardeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
